ca27047432eab9af066c6b22b903e69d5bee0870
sean-elliott-97 <sean.elliott.33197@gmail.com>
2022-02-13 17:12:21 -0500

    changed httpLink again

--- client/src/App.js
+++ client/src/App.js
@@ -12,7 +12,7 @@ import {
 import { setContext } from '@apollo/client/link/context';
 
 const httpLink = createHttpLink({
-  uri: 'http://localhost:3001/graphql',
+  uri: '/graphql',
 });
 const authLink = setContext((_, { headers }) => {
   const token = localStorage.getItem('id_token');
