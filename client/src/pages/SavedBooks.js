ccf014b6b320e1dc9506c9b570d4af2c7d3d6ee8
sean-elliott-97 <sean.elliott.33197@gmail.com>
2022-02-13 21:33:43 -0500

    fixed typo

--- client/src/pages/SavedBooks.js
+++ client/src/pages/SavedBooks.js
@@ -15,7 +15,7 @@ import { removeBookId } from "../utils/localStorage";
 const SavedBooks = () => {
   const {loading, data} = useQuery(GET_ME);
   const [removeBook, {error}] = useMutation(REMOVE_BOOK);
-  const userData = data?.me || {};
+  const userData = data?.me || [];
 
 
   // create function that accepts the book's mongo _id value as param and deletes the book from the database
