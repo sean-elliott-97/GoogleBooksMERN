655b6bcd043a937d638b5f4f8d3c809dcb4687b9
sean-elliott-97 <sean.elliott.33197@gmail.com>
2022-02-14 11:57:38 -0500

    changed server.js

--- server/server.js
+++ server/server.js
@@ -9,20 +9,13 @@ const PORT = process.env.PORT || 3001;
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 //apollo server
-const startServer = async ()=>{
 const server = new ApolloServer({
   typeDefs,
   resolvers,
   context: authMiddleware,
-
 });
-
-await server.start();
-
-server.applyMiddleware({ app });
 console.log(`Use GraphQL at http://localhost:${PORT}${server.graphqlPath}`);
-}
-startServer();
+server.applyMiddleware({ app });
 
 // if we're in production, serve client/build as static assets
 if (process.env.NODE_ENV === "production") {
